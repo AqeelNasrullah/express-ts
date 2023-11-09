706f4ad74f24ce5fceeae463caeef66fea6d61e9
Aqeel Nasrullah <aqeelnasrullah1997@gmail.com>
2023-11-09 20:06:23 +0500

    dist: removed dist folder from gitignore

---
+++ dist/index.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const http_1 = __importDefault(require("http"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const path_1 = __importDefault(require("path"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+app.use("/assets", express_1.default.static(path_1.default.join(__dirname, "../public")));
+app.get("/api", (req, res) => {
+    res.status(200).json({ message: "Server is up & running!!!" });
+});
+const BASE_URL = process.env.BASE_URL;
+const PORT = process.env.PORT;
+const NODE_ENV = process.env.NODE_ENV;
+const server = http_1.default.createServer(app);
+server.listen(PORT, () => console.log(`\nServer started:\n>> [HOST]: ${BASE_URL}\n>> [MODE]: ${NODE_ENV}\n`));
