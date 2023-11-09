ae1c5c47e7e0160ba9f1df1f2080dd402fa06d5b
Aqeel Nasrullah <aqeelnasrullah1997@gmail.com>
2023-11-09 20:03:07 +0500

    static delivery: added middleware for static content delivery

--- index.ts
+++ index.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import http from "http";
 import dotenv from "dotenv";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
+
+app.use("/assets", express.static(path.join(__dirname, "../public")));
 
 app.get("/api", (req, res) => {
   res.status(200).json({ message: "Server is up & running!!!" });
@@ -16,7 +19,7 @@ const NODE_ENV = process.env.NODE_ENV;
 
 const server = http.createServer(app);
 
-server.listen(8000, () =>
+server.listen(PORT, () =>
   console.log(
     `\nServer started:\n>> [HOST]: ${BASE_URL}\n>> [MODE]: ${NODE_ENV}\n`
   )
